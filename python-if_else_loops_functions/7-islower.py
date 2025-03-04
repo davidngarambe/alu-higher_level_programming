df5a9d4410c1ab797a45a6e81f3dfd761a2f23e5
davidngarambe <d.ngarambe@alustudent.com>
2025-03-04 13:57:38 -0800

    Complete

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
+
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
