258e01964d0b99a1f998426bd493410b7fbe58cc
davidngarambe <d.ngarambe@alustudent.com>
2025-03-04 14:12:54 -0800

    Complete

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        # Convert lowercase letters to uppercase using ASCII values
+        if 'a' <= char <= 'z':
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()  # Print a new line at the end
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
