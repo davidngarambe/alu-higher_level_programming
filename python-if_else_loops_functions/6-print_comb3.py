27e164b669aa05fcc45d33ff053360b4838f4ec9
davidngarambe <d.ngarambe@alustudent.com>
2025-03-04 09:53:19 -0800

    Changes made

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 9):  # Outer loop for the first digit
-    for j in range(i + 1, 10):  # Inner loop for the second digit
-        if i == 8 and j == 9:  # If it's the last pair, print without a comma
-            print(f"{i}{j}")
-        else:
-            print(f"{i}{j}", end=", ")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print(f"{i}{j}", end=", " if i != 8 or j != 9 else "\n")
