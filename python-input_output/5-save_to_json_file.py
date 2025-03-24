876ebdb5d441e8ba57e06b995ea756dcf218cec9
davidngarambe <d.ngarambe@alustudent.com>
2025-03-24 01:07:19 -0700

    Final

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,7 +8,11 @@ def save_to_json_file(my_obj, filename):
 
     Args:
         my_obj: The object to serialize and write to the file.
-        filename: The name of the file to write to.
+        filename (str): The name of the file to write to.
+
+    Example:
+        save_to_json_file([1, 2, 3], "my_list.json")
+        save_to_json_file({"name": "John", "age": 36}, "my_dict.json")
     """
     with open(filename, mode='w', encoding='utf-8') as file:
         json.dump(my_obj, file)
